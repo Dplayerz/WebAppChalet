3848cbf1a860cd6d2c64db30c61fdcb042801807
Dplayerz <xavpepper@icloud.com>
2023-11-15 20:52:15 -0500

    4commit

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,26 +1,5 @@
-import { DocumentData, onSnapshot, QuerySnapshot } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { PepperScore } from "./Pepper";
-import { NewType } from "./types/ScoreP";
+function Admin() {
+  <div>5555</div>;
+}
 
-const [scorePep, setScorePep] = useState<NewType[]>([]);
-useEffect(
-  () =>
-    onSnapshot(PepperScore, (snapshot: QuerySnapshot<DocumentData>) => {
-      setScorePep(
-        snapshot.docs.map((doc) => {
-          return {
-            PTS: doc.id,
-            ...doc.data(),
-          };
-        })
-      );
-    }),
-  []
-);
-
-const number = scorePep.map((item) => item.PTS);
-
-console.log("this is" + number);
-
-export default number;
+export default Admin;
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 // Filename - App.js
 import "./App.css";
 import "./assets/doom.jpg";
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./NavBar";
 //import Admin from "./Admin";
 import Leader from "./Leader";
-import Login from "./Login";
-import { useState } from "react";
-import Gang from "./Leader";
 
 function App() {
   return (
