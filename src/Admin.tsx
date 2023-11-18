4c06d9e02ee57e027503462ae319513aaa093a3d
Dplayerz <142250300+Dplayerz@users.noreply.github.com>
2023-11-18 14:43:36 -0500

    Update Admin.tsx

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,8 +1,41 @@
-import "./App.css"
+import "./styles.css";
+import "./App.css";
+import { team1 } from "./Leader";
+import { team2 } from "./Leader";
 
+const data = [
+  { Nom: "team1", PTS: team1 },
+  { Nom: "team2", PTS: team2 },
+];
+
+data.sort((a, b) => {
+  if (b.PTS < a.PTS) return -1;
+  if (b.PTS > a.PTS) return 1;
+  return 0;
+});
+import "./App.css";
 
 function Admin() {
- return <div>5555</div>;
+  return (
+    <div className="App">
+      <header>LEADERBOARD</header>
+
+      <table>
+        <tr>
+          <th>Nom</th>
+          <th>PTS</th>
+        </tr>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.Nom}</td>
+              <td>{val.PTS}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 }
 
 export default Admin;
