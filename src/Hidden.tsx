9619411a1f72fb812381859b355095e7e6ae8a4c
Dplayerz <142250300+Dplayerz@users.noreply.github.com>
2023-11-26 13:20:11 -0500

    Update Hidden.tsx

--- src/Hidden.tsx
+++ src/Hidden.tsx
@@ -11,6 +11,7 @@ function Hidden() {
         Defi secret: Screenshot ce gif et enlever le url avant l'envoi dans le
         groupe. "Un URL c'est le nom du site dans la bar du haut"
       </h2>
+      <h3>Merci pour votre participation!</h3>
     </div>
   );
 }
