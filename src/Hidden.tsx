e2ea5cd34812e4f1890ba1638b7b6e3071d3dd7e
Dplayerz <142250300+Dplayerz@users.noreply.github.com>
2023-11-26 13:13:10 -0500

    Update Hidden.tsx

--- src/Hidden.tsx
+++ src/Hidden.tsx
@@ -3,9 +3,15 @@ import "./Pouf.css";
 
 function Hidden() {
   return (
+    <div>
     <h1>
       <img src={gif} className="Pouf"></img>
     </h1>
+    <h2>
+        Defi secret: Screenshot ce gif et enlever le url avant l'envoi dans le
+        groupe. "Un URL c'est le nom du site dans la bar du haut"
+      </h2>
+    </div>
   );
 }
 
