98ea904b416ab66ad13296c709943a0d2247f8e5
Dplayerz <142250300+Dplayerz@users.noreply.github.com>
2023-12-04 20:06:13 -0500

    Update Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -56,7 +56,7 @@ const defi = [
   { D: "12", Nom: "AL, XP, YA" },
   { D: "13", Nom: "YA,MA,MeD,Eli" },
 {D: "E" , Nom: "ST,Emy,KA"},
-{D: "S1", Nom: "Yanny"},
+{D: "S1", Nom: "Yanny, Eli, Alexe"},
 {D: "S2", Nom: "Emy, Mari, Alexe"},
 {D: "S3", Nom: ""},
 {D: "S4", Nom: ""},
