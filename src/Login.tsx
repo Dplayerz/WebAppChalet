1a2fc2d444b06b2753701ddea447291d9ffe70f0
Dplayerz <142250300+Dplayerz@users.noreply.github.com>
2024-01-19 09:55:28 -0500

    Update Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -4,23 +4,15 @@ function Login() {
   return (
     <div>
     <ol>
-      <li>Prenez une photo de vous avec la croix du Mont Bellevue allumée.</li>
-      <li>
-        Prenez une photo de chaque participant aux olympiades à leur insu.
-        Chaque personne vaut 1pts. 1 photo par personne par équipe.
-      </li>
-      <li>Trouver une maison avec l'adresse civique 69 (No google maps)</li>
-      <li>Trouver les balles de pétanques au chalet.</li>
+      <li>Photographier un bambi</li>
+      <li>Trouver les balles de pétanques </li>
+      <li>Photographier un dindon </li>
+      <li>Trouver la vierge marie</li>
       <li>Apporter une carotte déjà cuite au chalet.</li>
-      <li>Vidéo de vous faisant un sport d’hiver au choix.</li>
-      <li> Le shotgun d’une seltzer complète avec preuve qu’elle est vide.</li>
+      <li>selfie avec quelqu'un qui dort</li>
+      <li>Trouver une golden can</li>
       <li>Glisser sur le ventre avec une crazy-carpet/soucoupe</li>
-      <li>Un selfie à 3h34 du matin, avec preuve</li>
-      <li>Une photo avec un employé d’un IGA</li>
-      <li>Faire une pizza avec de l' ananas.</li>
-      <li>Vidéo de vous qui Bench.</li>
-      <li>Faire un bonhomme de neige</li>
-      <li>A suivre.</li>
+      <li>Boire dans le baril d'or</li>
     </ol> 
           
 <table>
